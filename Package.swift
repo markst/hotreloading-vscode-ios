4f853e76b28ac7214616b737e47ab8bb3893a960
Mark Turner <git@markst.co.uk>
2022-06-02 13:03:23 +0200

    Force `sourcekit-lsp` not to display macOS errors.

--- Package.swift
+++ Package.swift
@@ -1,11 +1,19 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let packageName = "Demo"
+
+// Necessary for `sourcekit-lsp` support in VSCode:`
+
 let package = Package(
-  name: "",
-  platforms: [.iOS("14.0")],
+  name: packageName,
+  platforms: [
+    .iOS(.v14),
+    // disable building on macOS
+    .macOS("99.0"),
+  ],
   products: [
     .library(name: packageName, targets: [packageName])
   ],
